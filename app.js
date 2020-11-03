cb58ba89c8e7a0df42a0821a7fca0e5976790dcb
MazenAmria <t.mazenamria@gmail.com>
2020-11-03 21:42:47 +0200

    First Commit...

---
+++ app.js
@@ -0,0 +1,81 @@
+const mysql = require("mysql2");
+const express = require("express");
+
+// express app
+const app = express();
+
+// register view engine
+app.set('view engine', 'ejs');
+
+// listen for request
+app.listen(3000);
+
+// Middleware & Static Files
+app.use(express.static('assets'));
+app.use(express.urlencoded({extended:true}));
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "1234",
+    database: "test"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log('connected');
+});
+
+app.get('/', (_req, res) => {
+    con.query('SELECT * FROM items', (err, result, fields) => {
+        if(err) res.status(500).send(err);
+        else{
+            let columns = [];
+            for(let i = 0; i < fields.length; i++){
+                columns.push(fields[i].name);
+            }
+            res.render('prototype', {tableData:result , tableColumns:columns});
+        }
+    });
+});
+
+app.post('/edit', (req, res) => {
+    const editobj = req.body;
+    let itemId = parseInt(editobj.itemId),
+        price = parseInt(editobj.price),
+        quantity = parseInt(editobj.quantity),
+        itemName = editobj.itemName;
+
+    con.query(
+        `UPDATE items SET itemName = '${itemName}', price = ${price}, quantity = ${quantity} WHERE itemId = ${itemId};`,
+        (err) => {
+            if(err) res.status(500).send(err);
+            else res.end();
+        }
+    );
+});
+app.post('/add', (req, res) => {
+    const editobj = req.body;
+    let itemId = parseInt(editobj.itemId),
+        price = parseInt(editobj.price),
+        quantity = parseInt(editobj.quantity),
+        itemName = editobj.itemName;
+        
+    con.query(
+        `INSERT INTO items (itemName, itemId, price, quantity) VALUES('${itemName}', ${itemId}, ${price}, ${quantity})`,
+        (err) => {
+            if(err) res.status(500).send(err);
+            else res.end();
+        }        
+    );
+});
+app.delete('/delete/:id', (req, res) => {
+    const id = req.params.id;
+    con.query(
+        `DELETE FROM items WHERE itemId = ${id};`,
+        (err) => {
+            if(err) res.status(500).send(err);
+            else res.end();
+        }
+    );
+});
---
+++ assets/js/main.js
@@ -0,0 +1,98 @@
+$(document).ready(function(){
+    let eventHandlers = function(){
+        var $row = $(this);
+        $row.find(".editRow").on('click', function(){
+            $row.find(".editRow, .deleteRow").css("display", "none");
+            $row.find(".fEditRow, .cEditRow").css("display", "inline-block");
+            $row.find("td").not(":last-child").each(function(){
+                $(this).html(`<input name='${$(this).attr('name')}' value='${$(this).text()}'><input class='original-data' value='${$(this).text()}' style='display: none;'>`);
+            });
+        });
+        $row.find(".cEditRow").on('click', function(){
+            $row.find(".editRow, .deleteRow").css("display", "inline-block");
+            $row.find(".fEditRow, .cEditRow").css("display", "none");
+            $row.find("td").each(function(){
+                $(this).text($(this).find(".original-data").val());
+            });
+        });
+        $row.find(".fEditRow").on('click', function(){
+            let obj = {};
+            $row.find("input").each(function(){
+                obj[($(this).attr('name'))] = $(this).val();
+            });
+            $.ajax({
+                url: `/edit`,
+                type: `POST`,
+                data: obj,
+                success: function() {
+                    $row.find("td").each(function(){
+                        $(this).text($(this).find("input").val());
+                    });
+                },
+                error: function(err) {
+                    $row.find("td").each(function(){
+                        $(this).text($(this).find(".original-data").val());
+                    });
+                    alert(`Failed to edit item: status ` + err.status);
+                }
+            });
+            $row.find(".editRow, .deleteRow").css("display", "inline-block");
+            $row.find(".fEditRow, .cEditRow").css("display", "none");
+        });
+        $row.find(".deleteRow").on('click', function(){
+            let itemId = $row.find("td[name=itemId]").text();
+            $.ajax({
+                url: `/delete/${itemId}`,
+                type: 'DELETE',
+                success: function() {
+                    $row.remove();
+                },
+                error: function(err) {
+                    alert(`Failed to delete item: status ` + err.status);
+                }
+            });
+        });
+    }
+    $(".addr").each(function(){
+        var $row = $(this);
+        let obj = {};
+        $(".add-new").on('click', function(){
+            let addnew = "<tr class='clearfix addednew'>";
+            $row.find("input").each(function(){
+                obj[$(this).attr('name').trim()] = $(this).val();
+                addnew += `<td name="${$(this).attr('name').trim()}">${$(this).val()}</td>`;
+            });
+            addnew += `
+                <td>
+                    <button class='editRow'>
+                        <i class="fas fa-pencil-alt"></i>
+                    </button>
+                    <button class="fEditRow">
+                        <i class="fas fa-check"></i>
+                    </button>
+                    <button class="cEditRow">
+                        <i class="fas fa-times"></i>
+                    </button>
+                    <button class='deleteRow'>
+                        <i class="fas fa-trash-alt"></i>
+                    </button>
+                </td>
+            </tr>
+            `;
+            $.ajax({
+                url: `/add`,
+                type: `POST`,
+                data: obj,
+                success: function() {
+                    $(addnew).insertBefore($(".table tbody .addr"));
+                    $(".table tbody .addr input").val("");
+                    $(".table tbody tr.addednew").each(eventHandlers);
+                },
+                error: function(err) {
+                    alert(`Failed to add item: status ` + err.status);
+                }
+            });            
+        });
+    });
+    $(".table tbody tr").not(".addr").each(eventHandlers);
+});
